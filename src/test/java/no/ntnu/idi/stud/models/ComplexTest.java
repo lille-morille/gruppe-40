5b6bfff5f7eeb5ace5a922255c9463be3a21172a
Nathalie Graidy Andreassen <nathalga@stud.ntnu.no>
2024-02-07 17:22:19 +0100

    Tests model classes

---
+++ src/test/java/no/ntnu/idi/stud/models/ComplexTest.java
@@ -0,0 +1,49 @@
+package no.ntnu.idi.stud.models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ComplexTest {
+  @Nested
+  @DisplayName("Tests the complex constructor")
+  class ConstructorTest {
+    @Test
+    void constructor() {
+      Complex complex = new Complex(1, 2);
+      assertEquals(complex.getX0(), 1);
+      assertEquals(complex.getX1(), 2);
+    }
+  }
+
+  @Nested
+  @DisplayName("Tests the square root method")
+  class SqrtTest {
+    @Test
+    void sqrt() {
+      Complex complex = new Complex(0.1, -0.4);
+      Complex sqrtResult = complex.sqrt();
+      Complex result = new Complex(0.509, -0.395);
+      assert Math.abs(sqrtResult.getX0() - result.getX0()) < 0.01 &&
+          Math.abs(sqrtResult.getX1() - result.getX1()) < 0.01 : "Wrong answer!";
+    }
+    @Test
+    void sqrt1() {
+      Complex complex = new Complex(3, 4);
+      Complex sqrtResult = complex.sqrt();
+      Complex result = new Complex(2, 1);
+      assert Math.abs(sqrtResult.getX0() - result.getX0()) < 0.01 &&
+          Math.abs(sqrtResult.getX1() - result.getX1()) < 0.01 : "Wrong answer!";
+    }
+    @Test
+    void sqrt2() {
+      Complex complex = new Complex(42, 56);
+      Complex sqrtResult = complex.sqrt();
+      Complex result = new Complex(7.483, 3.742);
+      assert Math.abs(sqrtResult.getX0() - result.getX0()) < 0.01 &&
+          Math.abs(sqrtResult.getX1() - result.getX1()) < 0.01 : "Wrong answer!";
+    }
+  }
+}
