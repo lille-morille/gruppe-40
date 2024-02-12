5cc94d775f063a54dd49a59cbd7e271fae182371
Anders Noel Lothe Morille <anmorill@stud.ntnu.no>
2024-02-12 10:05:12 +0100

    Resolve "Implement more tests for JuliaTransform"

--- src/test/java/no/ntnu/idi/stud/transformations/JuliaTransformTest.java
+++ src/test/java/no/ntnu/idi/stud/transformations/JuliaTransformTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import no.ntnu.idi.stud.models.Complex;
 import no.ntnu.idi.stud.models.Vector2D;
 import org.junit.jupiter.api.Test;
+import no.ntnu.idi.stud.TestHelper;
 
 class JuliaTransformTest {
   @Test
@@ -49,9 +50,37 @@ class JuliaTransformTest {
 
     Vector2D result = transform.transform(z);
 
-    Vector2D correctResult = new Complex(0.506, -0.395);
+    Vector2D actual = new Complex(0.506, -0.395);
 
-    assert Math.abs(result.getX0() - correctResult.getX0()) < 0.01 &&
-        Math.abs(result.getX1() - correctResult.getX1()) < 0.01 : "Wrong answer!";
+    TestHelper.assertApproximateVectors(actual, result);
   }
+
+  @Test
+  void testTransformMethod2() {
+    Complex c = new Complex(6, -3);
+    Complex z = new Complex(3, 2);
+
+    JuliaTransform transform = new JuliaTransform(c, -1);
+
+    Vector2D result = transform.transform(z);
+
+    Vector2D expected = new Complex(-1.18974, -2.10130);
+
+    TestHelper.assertApproximateVectors(expected, result);
+  }
+
+  @Test
+  void testTransformMethod3() {
+    Complex c = new Complex(4, 4.5);
+    Complex z = new Complex(10, -8);
+
+    JuliaTransform transform = new JuliaTransform(c, 1);
+
+    Vector2D result = transform.transform(z);
+
+    Vector2D expected = new Complex(3.1516, -1.9831);
+
+    TestHelper.assertApproximateVectors(expected, result);
+  }
+
 }
