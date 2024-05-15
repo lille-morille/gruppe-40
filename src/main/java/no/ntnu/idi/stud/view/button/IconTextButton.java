8f4ed3db7b6e8e63c576d4a323da7e8228657111
Anders Morille <anmorill@stud.ntnu.no>
2024-05-15 21:44:39 +0200

    Buttons for saving and creating files

--- src/main/java/no/ntnu/idi/stud/view/button/IconTextButton.java
+++ src/main/java/no/ntnu/idi/stud/view/button/IconTextButton.java
@@ -1,5 +1,6 @@
 package no.ntnu.idi.stud.view.button;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import no.ntnu.idi.stud.view.Icon;
@@ -19,16 +20,18 @@ public class IconTextButton extends HBox implements StyledComponent {
    */
   public IconTextButton(String text, String icon,
                     javafx.event.EventHandler<? super javafx.scene.input.MouseEvent> onClick) {
+    super(5);
     addStylesheet("icon-text-button");
     getStyleClass().add("icon-text-button");
 
     Label label = new Label(text);
     label.getStyleClass().add("label");
     Icon iconView = new Icon(icon);
-    iconView.fitWidthProperty().setValue(10);
-    iconView.fitHeightProperty().setValue(10);
+    iconView.fitWidthProperty().setValue(15);
+    iconView.fitHeightProperty().setValue(15);
 
     getChildren().addAll(iconView, label);
+    setAlignment(Pos.CENTER);
 
     setOnMouseClicked(onClick);
   }
--- src/main/java/no/ntnu/idi/stud/view/sidebar/section/files/FilesSection.java
+++ src/main/java/no/ntnu/idi/stud/view/sidebar/section/files/FilesSection.java
@@ -1,9 +1,14 @@
 package no.ntnu.idi.stud.view.sidebar.section.files;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.TitledPane;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import no.ntnu.idi.stud.view.StyledComponent;
+import no.ntnu.idi.stud.view.button.IconTextButton;
 import no.ntnu.idi.stud.view.sidebar.filetree.custom.CustomFileTreeController;
 import no.ntnu.idi.stud.view.sidebar.filetree.custom.CustomFileTreeView;
 import no.ntnu.idi.stud.view.sidebar.filetree.templates.TemplatesFileTree;
@@ -19,6 +24,9 @@ public class FilesSection extends TitledPane implements StyledComponent {
     addStylesheet("sidebar/section/section");
     this.setText("Files");
 
+    VBox content = new VBox();
+
+    // Trees
     TreeView<String> treeView = new TreeView<>();
     treeView.showRootProperty().set(false);
 
@@ -33,7 +41,30 @@ public class FilesSection extends TitledPane implements StyledComponent {
 
     rootItem.getChildren().addAll(templatesView, customView);
     treeView.setRoot(rootItem);
+    content.getChildren().add(treeView);
 
-    this.setContent(treeView);
+    // Buttons
+
+    HBox buttonGroup = new HBox(20);
+    buttonGroup.setAlignment(Pos.CENTER);
+
+    IconTextButton newFileButton =
+        new IconTextButton("New File", "plus", this::handleNewFileClicked);
+    IconTextButton saveButton =
+        new IconTextButton("Save", "download", this::handleSaveClicked);
+
+    buttonGroup.getChildren().addAll(newFileButton, saveButton);
+
+    content.getChildren().add(buttonGroup);
+
+    this.setContent(content);
+  }
+
+  void handleNewFileClicked(MouseEvent event) {
+
+  }
+
+  void handleSaveClicked(MouseEvent event) {
+
   }
 }
